5a1f7cf1df21a4a5525f5519ee15956ecb0462a4
uzinok <johndoe@example.com>
2024-06-20 06:12:19 +0300

    добавляет проверку на палиндром

--- js/main.js
+++ js/main.js
@@ -8,3 +8,24 @@ checkLengthString('проверяемая строка', 20); // true
 checkLengthString('проверяемая строка', 18); // true
 // Строка длиннее 10 символов
 checkLengthString('проверяемая строка', 10); // false
+
+function checkPalindrome(string) {
+  let newString = '';
+
+  string = string.replaceAll(' ', '').toLowerCase();
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+
+  return newString === string;
+}
+
+// Строка является палиндромом
+checkPalindrome('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+checkPalindrome('ДовОд'); // true
+// Это не палиндром
+checkPalindrome('Кекс'); // false
+// Это палиндром
+checkPalindrome('Лёша на полке клопа нашёл '); // true
