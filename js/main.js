518bcf4d72782185311b95575863aa8a4c561601
uzinok <johndoe@example.com>
2024-06-20 05:28:14 +0300

    добавляет функцию проверки длины строки

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function checkLengthString(scring, length) {
+  return scring.length <= length;
+}
+
+// Строка короче 20 символов
+checkLengthString('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+checkLengthString('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+checkLengthString('проверяемая строка', 10); // false
