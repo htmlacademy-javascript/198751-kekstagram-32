8b9390122b03d23ad7290310373ee3afca182d93
uzinok <johndoe@example.com>
2024-07-13 12:42:33 +0300

    1

--- js/draw-comments.js
+++ js/draw-comments.js
@@ -2,7 +2,7 @@ const drawComments = (parentBlock, data) => {
 
   parentBlock.addEventListener('click', (e) => {
 
-    if (e.target.tagName === 'IMG' || e.target.tagName === 'SPAN' || e.target.tagName === 'P') {
+    if (e.target.closest('a')) {
       e.preventDefault();
     }
 
