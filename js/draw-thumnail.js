b3d90d48b607b178cb8fbfe6fe1eb3738eceb345
uzinok <41639106+uzinok@users.noreply.github.com>
2024-07-13 13:13:12 +0300

    переносит data-id к ссылке превью

--- js/draw-thumnail.js
+++ js/draw-thumnail.js
@@ -6,9 +6,9 @@ const drawThumnail = (data, picturesBlock) => {
 
   data.forEach(({ id, url, description, likes, comments }) => {
     const newPhoto = templateContent.cloneNode(true);
+    newPhoto.querySelector('.picture').dataset.id = id;
     newPhoto.querySelector('.picture__img').src = url;
     newPhoto.querySelector('.picture__img').alt = description;
-    newPhoto.querySelector('.picture__img').dataset.id = id;
     newPhoto.querySelector('.picture__likes').innerText = likes;
     newPhoto.querySelector('.picture__comments').innerText = comments.length;
 
