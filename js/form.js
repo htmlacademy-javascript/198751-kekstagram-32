24a77972f724f1f64475b9e05c7468039150dc20
uzinok <johndoe@example.com>
2024-07-29 04:23:41 +0300

    1

--- js/form.js
+++ js/form.js
@@ -11,22 +11,46 @@ const options = {
   errorTextParent: 'img-upload__field-wrapper'
 };
 
-// const isGetErrorDescription = (value) => {
-//   if (value.length > MAX_COMMENTS_LENGTH) {
-//     return `длина комментария не может составлять больше ${MAX_COMMENTS_LENGTH} символов`;
-//   }
-// };
+const descriptionMessage = `длина комментария не может составлять больше ${MAX_COMMENTS_LENGTH} символов`;
+const isErrorDescription = (value) => {
+  let isValid = true;
+
+  if (value.length > MAX_COMMENTS_LENGTH) {
+    isValid = false;
+  }
+
+  return isValid;
+};
+
+let hashtagsMessage = 'Поле заполнено некорректно.';
+const isErrorHashtag = (value) => {
+  value = value.split(' ');
+  let isValid = true;
+
+  if (value.length > 5) {
+    hashtagsMessage = 'asdasda';
+    isValid = false;
+    return isValid;
+  }
+
+  isValid = true;
+  [...value].forEach((elem) => {
+    if (elem > 2) {
+      hashtagsMessage = 'asdasda';
+      isValid = false;
+      return isValid;
+    }
+  });
+  return isValid;
+  // const test = /(#[А-яЁёA-z0-9]{1,19})/g;
+};
+
 
 const form = () => {
   const pristine = new Pristine(uploadFormImg, options, true);
 
-  pristine.addValidator(textDescription, (value) => {
-    if (value.length > MAX_COMMENTS_LENGTH) {
-      return true;
-    }
-    return false;
-  }, `длина комментария не может составлять больше ${MAX_COMMENTS_LENGTH} символов`, 2, false);
-
+  pristine.addValidator(textDescription, isErrorDescription, descriptionMessage);
+  pristine.addValidator(textHashtag, isErrorHashtag, hashtagsMessage);
 };
 
 export {
